2d1010eb74d115998169a1ff0841432090cc9955
rafaeliacksback2003@gmail.com <05589099064@senacrs.edu.br>
2025-10-28 16:19:48 -0300

    Criar uma função que cria um numero aleatorio entre 0 e 10

---
+++ ex1.ts
@@ -0,0 +1,8 @@
+// Fazer uma função que cria um novo array com os elementos do array original
+// invertidos
+
+const arr = [1, 2, 3, 4, 5];
+const arrInvertido = arr.reverse();
+console.log(arrInvertido);
+
+
---
+++ ex2.ts
@@ -0,0 +1,6 @@
+// Criar uma função que cria um numero aleatorio entre 0 e 10
+
+const numAleatorio = Math.floor(Math.random() * 11);
+console.log(numAleatorio);
+
+    
